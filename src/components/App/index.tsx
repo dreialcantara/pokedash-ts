3900d347a6d9d6542c54aa0f35823ef71e5687a6
Francisross Soares de Oliveira <franciscpd@gmail.com>
2022-08-27 12:17:28 -0300

    feat: add pokedash

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,14 +1,63 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState, useMemo } from 'react'
+import { CardGroup, Container } from 'react-bootstrap'
+
+import api from '../../services/api'
+import { Pokemon, PokemonResult } from '../../types'
+import Header from '../Header'
+import Card from '../Card'
+
+import '../../assets/styles/app.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<Array<Pokemon>>([])
+
+  const pokemonGroups = useMemo<Array<Array<Pokemon>> | []>(() => {
+    return data.reduce((dataSlice, item, index) => {
+      const chunkIndex = Math.floor(index / 3)
+
+      if (!dataSlice[chunkIndex]) {
+        (dataSlice[chunkIndex] as Array<never>) = []
+      }
+
+      (dataSlice[chunkIndex] as Array<Pokemon>).push(item)
+
+      return dataSlice
+    }, [])
+  }, [data]);
+
+  const fetchPokemons = useCallback(async () => {
+    const response = await api.get('/pokemon?limit=12').then((res) => {
+      return res.data.results
+    })
+
+    const promisesGetDetails = response.map(async (pokemon: PokemonResult) => {
+      return api.get(`/pokemon/${pokemon.name}`).then((res) => {
+        return res.data
+      })
+    })
+
+    const results = await Promise.all(promisesGetDetails)
+
+    setData(results)
+  }, []);
+
+  useEffect(() => {
+    fetchPokemons()
+  }, [fetchPokemons])
 
   return (
-    <div className="App">
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </div>
+    <>
+      <Header />
+      <Container fluid="lg">
+        {pokemonGroups.map((group: Array<Pokemon>, index) => (
+          <CardGroup key={index} className="mb-3">
+            {group.map((pokemon: Pokemon) => (
+              <Card key={pokemon.id} pokemon={pokemon} />
+            ))}
+          </CardGroup>
+        ))}
+      </Container>
+    </>
   )
 }
 
